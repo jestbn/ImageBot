9af090576ee650c24db34f7ab4f3d765b325d868
Juan Esteban Gil <juanestebang9@gmail.com>
2020-02-25 21:40:20 -0500

    Agregar archivos de proyecto.

---
+++ ImageBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
